b813f7fa3a515cee898762ec0b10bc6db4c1657a
Antoni Zajko <antoni.zajko.1@gmail.com>
2025-12-01 19:02:34 +0100

    Add boilerplate main

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,7 +1,60 @@
+#include <string>
+#include <vector>
+#include <memory>
 #include "empty.hpp"
 
+// Moduł config
+class Config {};
+class Parser {
+    public:
+        Config parseConfig(std::string configPath);
+};
 
-int main()
-{
-    return fun(1,2);
+// Output
+class Output {
+
+};
+
+// Moduł gry
+class Game {
+    public:
+        Game(Config config);
+        Output play();
+};
+
+// Moduł strategii
+class Strategy {
+    public:
+        virtual int makeDecision(std::vector<int> gameHistoryWithPlayer) = 0;
+};
+
+// Moduł gracza
+class PlayerFactory {
+    Player makePlayer(...) { // TODO: do definiowania jakie parametry
+
+    }
+};
+
+class Player {
+    public:
+        Player(int id, std::shared_ptr<Strategy> strategy): id(id), strategy(strategy) {}
+        int getId() { return id; }
+    
+    private:
+        int id;
+        std::shared_ptr<Strategy> strategy;
+};
+
+
+// Ewaluator
+
+
+
+int main() { // TODO: dodac argsy
+    auto configPath = "";
+    auto parser = Parser();
+    auto config = parser.parseConfig(configPath); // configPath z argsów
+    auto game = Game(config);
+    game.play();
+
 }
