f25acdf4c26e0555c5253cb997b43bda2731afb7
Michał Obara <michal.obara@bcbj.gov.pl>
2025-12-08 17:58:58 +0100

    Dodanie modułu Config i przykładowy config.yaml

---
+++ build/prison/CMakeFiles/prison_lib.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mobara/dev/studia/sylvine/source/prison/src/empty.cpp" "prison/CMakeFiles/prison_lib.dir/src/empty.cpp.o" "gcc" "prison/CMakeFiles/prison_lib.dir/src/empty.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
